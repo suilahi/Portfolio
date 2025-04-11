47fd70f779144b7592355bc0ba0e60a06eec2f8a
suilahi <suilahizakaria@gmail.com>
2025-04-11 12:45:01 +0100

    contact component part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjetComponent } from './projet/projet.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,HomeComponent,AboutComponent,ProjetComponent],
+  imports: [RouterOutlet,NavbarComponent,HomeComponent,AboutComponent,ProjetComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
